28c0bbe2502c579ec21abc4d7417533d3e882053
Joachim MASSON <joachim.masson.17@gmail.com>
2026-05-11 09:51:43 +0200

    add link for LegalNotice in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ function Footer () {
   return (
   <footer className="Footer">
     <p className="Footer-p"> copyright © 2026 / Joachim Masson </p>
+    <a className="Footer-a"href="../pages/LegalNotice.tsx">Mention légales</a>
     <p className="Footer-p">contact to: joachim.masson.17@gmail.com </p>
   </footer >
   )
